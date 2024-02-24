e75a321bfdc2d7e6bb78081d03b09fe8b3907a36
Nikita Sechnoy <NikitaSech@yandex.ru>
2024-02-25 00:44:38 +0300

    init: добавит тестовые исходники

---
+++ src/app/ui/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './app.scss';
+
+export default function App(): JSX.Element {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="header">Finger Trainer</h1>
+      <button className="button" type="button" onClick={() => setCount(count + 1)}>
+        Счетчик на
+        {' '}
+        {count}
+      </button>
+    </>
+  );
+}
