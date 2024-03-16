f60e6a0347835b97a37f4ac6aeb33c0989932739
Nikita Sechnoy <NikitaSech@yandex.ru>
2024-03-16 21:44:39 +0300

    refactor: разделяет роутер по файлам

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,26 +1,8 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import LessonsPage from '../../pages/lessons';
-import StatsPage from '../../pages/stats';
-import WorkoutPage from '../../pages/workout';
+import AppRouter from '../providers/router';
 import './app.scss';
-
-const routerConfig = createBrowserRouter([
-  {
-    path: '/',
-    element: <WorkoutPage />,
-  },
-  {
-    path: '/lessons',
-    element: <LessonsPage />,
-  },
-  {
-    path: '/stats',
-    element: <StatsPage />,
-  },
-]);
 
 export default function App(): JSX.Element {
   return (
-    <RouterProvider router={routerConfig} />
+    <AppRouter />
   );
 }
